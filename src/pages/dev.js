57217b5d1a0b076abf727a97eaf6cd8c44d60db7
annakap <anna.kaplan@skillshare.com>
2024-09-07 15:12:50 -0700

    updated site with 11ty

--- src/pages/dev.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class Dev extends Component {
-    render() {
-       return (<div>Dev</div>)
-    }
-}
-
-export default Dev;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{liquid,md}", "./src/**/*.svg",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
